ff332e3e2580d8a426be9deec8f8cc5c0a5c978f
ludik <bakhanetssimon@gmail.com>
2026-01-27 16:18:24 +0300

    Initial Commit

---
+++ X-LifeEnhance/X_LifeEnhanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  X_LifeEnhanceApp.swift
+//  X-LifeEnhance
+//
+//  Created by Simon Bakhanets on 27.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct X_LifeEnhanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
